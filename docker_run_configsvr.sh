90543275fc4dc707755bf0c07983f1972742c6e7
yekoufeng <357262356@qq.com>
2018-11-20 02:30:49 +0800

    add overlay

--- docker_run_configsvr.sh
+++ docker_run_configsvr.sh
@@ -1,3 +1,3 @@
-docker run -d --name configsvr0  -v /home/mongodb/data/cs/configsvr0:/data/configdb mongo --configsvr --replSet "rs_configsvr"  --bind_ip_all
-docker run -d --name configsvr1  -v /home/mongodb/data/cs/configsvr1:/data/configdb mongo --configsvr --replSet "rs_configsvr"  --bind_ip_all
-docker run -d --name configsvr2  -v /home/mongodb/data/cs/configsvr2:/data/configdb mongo --configsvr --replSet "rs_configsvr"  --bind_ip_all
+docker run -d --name configsvr0  --network mongodb_ov_net -v /home/mongodb/data/cs/configsvr0:/data/configdb mongo --configsvr --replSet "rs_configsvr"  --bind_ip_all
+docker run -d --name configsvr1  --network mongodb_ov_net -v /home/mongodb/data/cs/configsvr1:/data/configdb mongo --configsvr --replSet "rs_configsvr"  --bind_ip_all
+docker run -d --name configsvr2  --network mongodb_ov_net -v /home/mongodb/data/cs/configsvr2:/data/configdb mongo --configsvr --replSet "rs_configsvr"  --bind_ip_all
--- docker_run_shardsvr.sh
+++ docker_run_shardsvr.sh
@@ -1,6 +1,6 @@
-docker run --name shardsvr00 -d -v /home/mongodb/data/sh/shardsvr00:/data/db mongo --shardsvr --replSet "rs_shardsvr0"  --bind_ip_all
-docker run --name shardsvr01 -d -v /home/mongodb/data/sh/shardsvr01:/data/db mongo --shardsvr --replSet "rs_shardsvr0"  --bind_ip_all
-docker run --name shardsvr02 -d -v /home/mongodb/data/sh/shardsvr02:/data/db mongo --shardsvr --replSet "rs_shardsvr0"  --bind_ip_all
-docker run --name shardsvr10 -d -v /home/mongodb/data/sh/shardsvr10:/data/db mongo --shardsvr --replSet "rs_shardsvr1"  --bind_ip_all
-docker run --name shardsvr11 -d -v /home/mongodb/data/sh/shardsvr11:/data/db mongo --shardsvr --replSet "rs_shardsvr1"  --bind_ip_all
-docker run --name shardsvr12 -d -v /home/mongodb/data/sh/shardsvr12:/data/db mongo  --shardsvr --replSet "rs_shardsvr1"  --bind_ip_all
+docker run --name shardsvr00 -d --network mongodb_ov_net  -v /home/mongodb/data/sh/shardsvr00:/data/db mongo --shardsvr --replSet "rs_shardsvr0"  --bind_ip_all
+docker run --name shardsvr01 -d --network mongodb_ov_net  -v /home/mongodb/data/sh/shardsvr01:/data/db mongo --shardsvr --replSet "rs_shardsvr0"  --bind_ip_all
+docker run --name shardsvr02 -d --network mongodb_ov_net  -v /home/mongodb/data/sh/shardsvr02:/data/db mongo --shardsvr --replSet "rs_shardsvr0"  --bind_ip_all
+docker run --name shardsvr10 -d --network mongodb_ov_net  -v /home/mongodb/data/sh/shardsvr10:/data/db mongo --shardsvr --replSet "rs_shardsvr1"  --bind_ip_all
+docker run --name shardsvr11 -d --network mongodb_ov_net  -v /home/mongodb/data/sh/shardsvr11:/data/db mongo --shardsvr --replSet "rs_shardsvr1"  --bind_ip_all
+docker run --name shardsvr12 -d --network mongodb_ov_net  -v /home/mongodb/data/sh/shardsvr12:/data/db mongo  --shardsvr --replSet "rs_shardsvr1"  --bind_ip_all
