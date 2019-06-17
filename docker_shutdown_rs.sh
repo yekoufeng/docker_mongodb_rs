1fd9a3aa16aaf04db5392a76f4072f612c011b41
ye koufeng <357262356@qq.com>
2019-06-17 06:16:46 -0400

    docker_shutdown_rs.sh

--- docker_shutdown_rs.sh
+++ docker_shutdown_rs.sh
@@ -1,21 +1,21 @@
 docker stop mongos0
-docker rm mongos0
+docker rm -f  mongos0
 docker stop configsvr0
-docker rm configsvr0
+docker rm -f configsvr0
 docker stop configsvr1
-docker rm configsvr1
+docker rm -f configsvr1
 docker stop configsvr2
-docker rm configsvr2
+docker rm -f configsvr2
 docker stop shardsvr00
-docker rm shardsvr00
+docker rm -f shardsvr00
 docker stop shardsvr01
-docker rm shardsvr01
+docker rm -f shardsvr01
 docker stop shardsvr02
-docker rm shardsvr02
+docker rm -f shardsvr02
 docker stop shardsvr10
-docker rm shardsvr10
+docker rm -f shardsvr10
 docker stop shardsvr11
-docker rm shardsvr11
+docker rm -f shardsvr11
 docker stop shardsvr12
-docker rm shardsvr12
+docker rm -f shardsvr12
 
